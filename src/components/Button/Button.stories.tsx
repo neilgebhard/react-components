8d1df5a9716e22be2ebc0036b3f2edfa94774b1a
Neil Gebhard <neilgebhard@gmail.com>
2022-01-17 17:53:23 +1000

    :lipstick: Add sizes to stories of buttons

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -2,17 +2,32 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Button from "./Button";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "ReactComponents/Button",
   component: Button,
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Default = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Default.args = {
   label: "Button",
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  label: "Button",
+  size: "small",
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  label: "Button",
+  size: "medium",
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  label: "Button",
+  size: "large",
 };
